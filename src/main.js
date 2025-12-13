ef9986141462939a0cf9155dd925181a11d44c05
Михайло Тарасенко <mishabos.bos@gmail.com>
2025-12-13 22:23:35 +0200

    Added image files

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/partials.js';
+import './js/aboutus.js';
+import './js/faq';
+import './js/stories.js';
+import './js/mobile-menu.js';
+import './js/modal.js';
+import './js/modalpet.js';
