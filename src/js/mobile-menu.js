2fa294bb6278ed9548eac730f1b45d5f57838121
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2025-12-14 02:14:08 +0200

    header-screen-fix

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,23 +2,23 @@ const burgerBtn = document.querySelector('.burger-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeBtn = document.getElementById('data-burger-close');
 
-// Відкрити меню
+// відкрити меню
 burgerBtn.addEventListener('click', () => {
   mobileMenu.classList.add('open');
 });
 
-// Закрити меню
+// закрити меню
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('open');
 });
 
-// Закриття при кліку поза меню
+// закриття при кліку поза меню
 mobileMenu.addEventListener('click', (e) => {
   if (e.target === mobileMenu) {
     mobileMenu.classList.remove('open');
   }
 });
-// Закриття по клавіші ESC
+// закриття по клавіші ESC
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && mobileMenu.classList.contains('open')) {
     mobileMenu.classList.remove('open');
